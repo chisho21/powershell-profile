8b56f927e6d0472a7547e5ada91d1b7f2fa695ec
Evan Chisholm <chisho21@gmail.com>
2022-04-30 10:29:45 -0400

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -6,54 +6,36 @@ function Update-Powershell {
 
 
 Write-Host "Loading Pshell Prompt.." -ForegroundColor White -BackgroundColor DarkGreen
-function prompt {
-# .Description
-# This custom version of the PowerShell prompt will present a colorized location value based on the current provider. It will also display the PS prefix in red if the current user is running as administrator.    
-# .Link
-# https://go.microsoft.com/fwlink/?LinkID=225750
-# .ExternalHelp System.Management.Automation.dll-help.xml
-$user = [Security.Principal.WindowsIdentity]::GetCurrent()
-if ( (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) {
-    $adminfg = "Red"
-}
-else {
-    $adminfg = $host.ui.rawui.ForegroundColor
-}
-
-Switch ((get-location).provider.name) {
-    "FileSystem" { $fg = "green"}
-    "Registry" { $fg = "magenta"}
-    "wsman" { $fg = "cyan"}
-    "Environment" { $fg = "yellow"}
-    "Certificate" { $fg = "darkcyan"}
-    "Function" { $fg = "gray"}
-    "alias" { $fg = "darkgray"}
-    "variable" { $fg = "darkgreen"}
-    Default { $fg = $host.ui.rawui.ForegroundColor}
-} 
+Set-PoshPrompt -Theme remk
 
 ##Posh-Git stuff
 Import-Module posh-git
+Import-Module PSCredVault
+Import-PSCredVault
+Import-Module tzwinops
 
 ## PSReadlinesettings for fsh like prediections
 Import-Module psreadline 
 Set-PSReadLineOption -PredictionSource History
 
-##Output to host
-    Write-Host "[$((Get-Date).timeofday.tostring().substring(0,8))] " -NoNewline
-    Write-Host "PS " -nonewline -ForegroundColor $adminfg
-    Write-Host "$($executionContext.SessionState.Path.CurrentLocation)" -foregroundcolor $fg -nonewline
-Write-VcsStatus
-    Write-Output "$('>' * ($nestedPromptLevel + 1)) "
-
-}#end prompt
-
 
 # Set-Global Output variable
 
-Write-Host 'Setting global catch variable $__' -ForegroundColor White -BackgroundColor DarkGreen
-$PSDefaultParameterValues['Out-Default:OutVariable'] = '__'
-# Can be retrived by calling $__
+Write-Host 'Setting global catch variable $__ and other command defaults.' -ForegroundColor White -BackgroundColor DarkGreen
+
+# Default Pwsh Parameters
+$PSDefaultParameterValues = @{
+	'Export-Csv:NoTypeInformation' = $true
+	'ConvertTo-Csv:NoTypeInformation' = $true
+	'Out-Default:OutVariable'  = '__'
+	'Out-Default:ErrorVariable'  = 'lasterror'
+	'Enter-PSSession:Credential' = $admincred
+	'Get-ChildItem:Force' = $true
+	'Out-File:Encoding' = 'utf8'
+	'Receive-Job:Keep' = $true
+	'Format-Table:AutoSize' = $true
+	'Invoke-Command:Credential' = $admincred
+}
 
 
 #  Set Useful aliases
@@ -68,10 +50,14 @@ function cd.... { cd ..\..\.. }
 function md5    { Get-FileHash -Algorithm MD5 $args }
 function sha1   { Get-FileHash -Algorithm SHA1 $args }
 function sha256 { Get-FileHash -Algorithm SHA256 $args }
+function hashme ($string , $Algorithm = "md5"){
+$mystream = [IO.MemoryStream]::new([byte[]][char[]]$string)
+Get-FileHash -InputStream $mystream -Algorithm $Algorithm
+}
 
 # Quick shortcut to start notepad
 function n      { notepad $args }
-function npp { start-process -filepath 'C:\Program Files (x86)\Notepad++\notepad++.exe' -argumentlist $args }
+function npp { start-process -filepath 'C:\Program Files\Notepad++\notepad++.exe' -argumentlist $args }
 function vi { start-process -filepath 'C:\Program Files (x86)\Vim\vim82\vim.exe' -argumentlist $args }
 
 # Drive shortcuts
@@ -103,6 +89,15 @@ function Get-Table {
        Write-Host "$count items have been added to variable `$table"
    }
 
+function Keep-Awake {
+$wsh = New-Object -ComObject WScript.Shell
+while (1) {
+  # Send Shift+F15 - this is the least intrusive key combination I can think of and is also used as default by:
+  # http://www.zhornsoftware.co.uk/caffeine/
+  # Unfortunately the above triggers a malware alert on Sophos so I needed to find a native solution - hence this script...
+  $wsh.SendKeys('+{F15}')
+  Start-Sleep -seconds 59
+}
 
-  
 
+}
